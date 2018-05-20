c683d0c7afa821b7f3da743fc0668bc0bbeaa6d5
Charles Carey <ccar845@aucklanduni.ac.nz>
2018-05-20 15:03:40 +1200

    Add test suite for business validation functionality

---
+++ src/test/java/unit/TestBusinessIdeaValidation.java
@@ -0,0 +1,23 @@
+package unit;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+public class TestBusinessIdeaValidation {
+
+    @Test
+    public void testWeightingOfSearchCategories() {
+        fail();
+    }
+
+    @Test
+    public void testAssigningRelevanceOfSearchCategories() {
+        fail();
+    }
+
+    @Test
+    public void testCalculatingBusinessMaturityScore() {
+        fail();
+    }
+}
