b7570fdd36dc3195d36803c096ef6e3f8b895513
Max Griffith <mgriffith37@gmail.com>
2018-05-20 16:19:16 +1200

    Implement test to return documents from search

--- src/test/java/unit/TestMarketComprehension.java
+++ src/test/java/unit/TestMarketComprehension.java
@@ -1,28 +1,63 @@
 package unit;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestMarketComprehension {
 
+    @Before
+    public void setup(){
+        SearchService _searchService = mock(SearchService.class);
+        when(_searchService.search(any())).thenReturn(generateDocumentList());
+        DocumentHandler _docHandler= new DocumentHandler();
+
+    }
+
     @Test
-    public void TestSearchReturnDocuments(){
+    public void testSearchReturnDocuments(){
+        String[] keywords = {"Ponsonby", "Dog", "Walking"};
+        SearchQuery query = new SearchQuery(keywords);
+        ArrayList<Document> resultDocs = _searchService.search(query);
+
+        Assert.assertTrue(resultDocs.size() == 10);
+        Assert.assertTrue(resultDocs.get(0).name().equals("Ponsonby Dog Walking Inc"));
         fail();
     }
 
     @Test
-    public void TestDocumentClustering(){
+    public void testDocumentClustering(){
         fail();
     }
 
     @Test
-    public void TestCategoryLabelExtraction(){
+    public void testCategoryLabelExtraction(){
         fail();
     }
 
     @Test
-    public void TestCategorySummaryGeneration(){
+    public void testCategorySummaryGeneration(){
         fail();
+    }
+
+    private ArrayList<Document> generateDocumentList() {
+        ArrayList<Document> docs = new ArrayList<Document>();
+        docs.add(new Document("Ponsonby Dog Walking Inc"));
+        docs.add(new Document("Newmarket Dog Walking Inc"));
+        docs.add(new Document("Parnell Dog Walking Inc"));
+        docs.add(new Document("Mount Eden Dog Walking Inc"));
+        docs.add(new Document("Sandringham Dog Walking Inc"));
+        docs.add(new Document("Grafton Dog Walking Inc"));
+        docs.add(new Document("Eden Terrace Dog Walking Inc"));
+        docs.add(new Document("Albany Dog Walking Inc"));
+        docs.add(new Document("Kingsland Dog Walking Inc"));
+        docs.add(new Document("Epsom Dog Walking Inc"));
+        return docs;
     }
 }
