89d77c58bc2be621bf7afa433dea7686a3fde9ca
Fraser McIntosh <frasermcintosh1@gmail.com>
2018-05-20 15:16:04 +1200

    Create mock user database

--- src/test/java/unit/TestAccountManagement.java
+++ src/test/java/unit/TestAccountManagement.java
@@ -6,6 +6,20 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class TestAccountManagement {
+    Database _db;
+
+    @Before
+    public void setup() {
+        _db = mock(Database.class);
+        when(_db.get(anyString(), anyString()).thenReturn(null));
+
+        when(_db.add("user", "password", UserType.USER)).thenReturn(new User("user", UserType.USER));
+        when(_db.get("user", "password")).thenReturn(new User("user", UserType.USER));
+
+        when(_db.add("admin", "password", UserType.ADMIN).thenReturn(new User("admin", UserType.ADMIN));
+        when(_db.get("admin", "password")).thenReturn(new User("admin", UserType.ADMIN));
+    }
+
     @Test
     public void testUserSignUp() {
        fail();
