0dc80bc3bd1c322ce8e74b02df3aa6348c52c4a0
Max Griffith <mgriffith37@gmail.com>
2018-05-20 15:15:22 +1200

    Added Test class with 4 empty test cases for the Market Comprehension requirements

---
+++ src/test/java/unit/TestMarketComprehension.java
@@ -0,0 +1,28 @@
+package unit;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestMarketComprehension {
+
+    @Test
+    public void TestSearchReturnDocuments(){
+        fail();
+    }
+
+    @Test
+    public void TestDocumentClustering(){
+        fail();
+    }
+
+    @Test
+    public void TestCategoryLabelExtraction(){
+        fail();
+    }
+
+    @Test
+    public void TestCategorySummaryGeneration(){
+        fail();
+    }
+}
