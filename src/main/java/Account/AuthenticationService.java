3840fabe1287490dcc47262ca34f90a8b00b259e
Fraser McIntosh <frasermcintosh1@gmail.com>
2018-05-20 16:23:08 +1200

    Add logout and session management to compile logout tests

--- src/main/java/Account/AuthenticationService.java
+++ src/main/java/Account/AuthenticationService.java
@@ -1,7 +1,11 @@
 package Account;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class AuthenticationService {
     UserDatabase _db;
+    HashMap<User, UUID> sessions = new HashMap<User, UUID>();
 
     public AuthenticationService(UserDatabase db) {
         _db = db;
@@ -9,11 +13,19 @@ public class AuthenticationService {
 
     public User signUp(String username, String password, UserType type) {
         User newUser = new User(username, type);
+        sessions.put(newUser, UUID.randomUUID());
         _db.add(username, password, newUser);
         return newUser;
     }
 
     public User login(String username, String password) {
-       return _db.get(username, password);
+        User user = _db.get(username, password);
+        sessions.put(user, UUID.randomUUID());
+       return user;
+    }
+
+    public boolean logout(User user) {
+        sessions.remove(user);
+        return true;
     }
 }
