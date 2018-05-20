55e3bdcc1bd05d621a56e483743a1961ef422c1c
Fraser McIntosh <frasermcintosh1@gmail.com>
2018-05-20 15:13:55 +1200

    Create Account management test skeletons

---
+++ src/test/java/unit/TestAccountManagement.java
@@ -0,0 +1,48 @@
+package unit;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class TestAccountManagement {
+    @Test
+    public void testUserSignUp() {
+       fail();
+    }
+
+    @Test
+    public void testUserLogin() {
+        fail();
+    }
+
+    @Test
+    public void testNonExistentUserLogin() {
+        fail();
+    }
+
+    @Test
+    public void testAdminSignUp() {
+        fail();
+    }
+
+    @Test
+    public void testAdminLogin() {
+        fail();
+    }
+
+    @Test
+    public void testNonExistentAdminLogin() {
+        fail();
+    }
+
+    @Test
+    public void testLoggedInUserSignOut() {
+        fail();
+    }
+
+    @Test
+    public void testLoggedInAdminSignOut() {
+        fail();
+    }
+}
