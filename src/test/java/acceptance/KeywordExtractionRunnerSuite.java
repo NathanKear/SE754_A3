4db5ff9764b007b12896af59c4d38150b27fdf4c
Nathan Kear <n.j.kear@gmail.com>
2018-05-23 23:37:45 +1200

    Add test class to include JBehave acceptance tests in JUnit tests

---
+++ src/test/java/acceptance/KeywordExtractionRunnerSuite.java
@@ -0,0 +1,10 @@
+package acceptance;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ StoryRunner.class })
+public class KeywordExtractionRunnerSuite {
+
+}
